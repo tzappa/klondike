c8b53e020523a768b566dec82865c2376bd9516b
Plamen Popov <tzappa@gmail.com>
2024-11-27 16:11:49 +0200

    klondike

--- public/js/sol.js
+++ public/js/sol.js
@@ -1,56 +1,138 @@
-class Card {
-	constructor(suit, value, faceUp = true) {
-		if (suit == 'h' || suit == 'hearts' || suit == 'H' || suit == 'Hearts' || suit == '\u2665') {
-			this.suit = '\u2665';
-			this.suitCode = 'H';
-			this.suitName = 'Hearts';
-		} else if (suit == 'd' || suit == 'diamonds' || suit == 'D' || suit == 'Diamonds' || suit == '\u2666') {
-			this.suit = '\u2666';
-			this.suitCode = 'D';
-			this.suitName = 'Diamonds';
-		} else if (suit == 'c' || suit == 'clubs' || suit == 'C' || suit == 'Clubs' || suit == '\u2663') {
-			this.suit = '\u2663';
-			this.suitCode = 'C';
-			this.suitName = 'Clubs';
-		} else if (suit == 's' || suit == 'spades' || suit == 'S' || suit == 'Spades' || suit == '\u2660') {
-			this.suit = '\u2660';
-			this.suitCode = 'S';
-			this.suitName = 'Spades';
+class Suit {
+	constructor(symbol) {
+		if (symbol === '♥') {
+			this.name = 'Hearts';
+		} else if (symbol === '♦') {
+			this.name = 'Diamonds';
+		} else if (symbol === '♣') {
+			this.name = 'Clubs';
+		} else if (symbol === '♠') {
+			this.name = 'Spades';
 		} else {
 			throw new Error('Invalid suit');
 		}
-		this.value = value;
-		this.isFaceUp = faceUp;
+		this.symbol = symbol;
 	}
 
-	toString() {
-		return `${this.value}${this.suit}`;
+	static get HEARTS() {
+		return new Suit('♥');
+	}
+
+	static get DIAMONDS() {
+		return new Suit('♦');
+	}
+
+	static get CLUBS() {
+		return new Suit('♣');
+	}
+
+	static get SPADES() {
+		return new Suit('♠');
+	}
+
+	static fromString(suit) {
+		switch (suit.toLowerCase()) {
+			case 'h':
+			case 'hearts':
+			case '♥':
+				return Suit.HEARTS;
+			case 'd':
+			case 'diamonds':
+			case '♦':
+				return Suit.DIAMONDS;
+			case 'c':
+			case 'clubs':
+			case '♣':
+				return Suit.CLUBS;
+			case 's':
+			case 'spades':
+			case '♠':
+				return Suit.SPADES;
+			default:
+				throw new Error('Invalid suit');
+		}
+	}
+}
+
+class CardValue {
+	constructor(symbol) {
+		const validValues = {
+			'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+			'J': 11, 'Q': 12, 'K': 13, 'A': 1
+		};
+
+		if (!validValues[symbol]) {
+			throw new Error('Invalid card value');
+		}
+
+		this.symbol = symbol;
+		this.rank = validValues[symbol];
+	}
+
+	static fromString(value) {
+		return new CardValue(value);
+	}
+}
+
+class Card {
+	constructor(suit, value, faceUp = true) {
+		this.suit = Suit.fromString(suit);
+		this.value = CardValue.fromString(value);
+		this.isFaceUp = faceUp;
+
+		this.element = document.createElement('div');
+		this.element.classList.add('card');
+		this.element.classList.add(this.isFaceUp ? this.suit.name.toLowerCase() : 'facedown');
+		this.element.style.left = '0px';
+		this.element.style.top = '0px';
+		this.element.innerHTML = this.toString();
 	}
 
 	flip() {
 		this.isFaceUp = !this.isFaceUp;
+		this.element.innerHTML = this.toString();
+		if (this.isFaceUp) {
+			this.element.classList.remove('facedown');
+			this.element.classList.add(this.suit.name.toLowerCase());
+		} else {
+			this.element.classList.add('facedown');
+			this.element.classList.remove(this.suit.name.toLowerCase());
+		}
 	}
 
-	toHtml() {
-		const cardClass = this.isFaceUp ? this.suitName.toLowerCase() : 'facedown';
-		const cardContent = this.isFaceUp ? this.toString() : '?';
-		return `<span class="${cardClass}">${cardContent}</span>`;
+	pos(left, top) {
+		this.element.style.left = `${left}px`;
+		this.element.style.top = `${top}px`;
+	}
+
+	parent(element) {
+		element.appendChild(this.element);
+	}
+
+	toString() {
+		return this.isFaceUp ? `${this.value.symbol}${this.suit.symbol}` : '?';
+	}
+
+	_toHtml() {
+		const cardClass = this.isFaceUp ? this.suit.name.toLowerCase() : 'facedown';
+		return `<div class="card ${cardClass}">${this.toString()}</div>`;
 	}
 }
 
 class Deck {
-	constructor() {
+	constructor(faceUp = false) {
 		this.cards = [];
+		this.faceUp = faceUp;
 		this.initializeDeck();
 	}
 
 	initializeDeck() {
-		const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+		const suits = ['♥', '♦', '♣', '♠'];
 		const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 		for (let suit of suits) {
 			for (let value of values) {
-				this.cards.push(new Card(suit, value));
+				this.cards.push(new Card(suit, value, this.faceUp));
 			}
 		}
 	}
@@ -73,16 +155,24 @@ class Deck {
 }
 
 class Pile {
-	constructor() {
+	constructor(parent, id) {
 		this.cards = [];
+
+		this.element = document.createElement('div');
+		this.element.id = id;
+		this.element.classList.add('pile');
+		parent.appendChild(this.element);
 	}
 
 	addCard(card) {
 		this.cards.push(card);
+		card.parent(this.element);
 	}
 
 	removeCard() {
-		return this.cards.pop();
+		let card = this.cards.pop();
+		card.pos(0, 0);
+		return card;
 	}
 
 	topCard() {
@@ -92,106 +182,98 @@ class Pile {
 	isEmpty() {
 		return this.cards.length === 0;
 	}
+}
 
-	toHtml() {
-		return this.cards.map(card => card.toHtml()).join('');
+class Tableau extends Pile {
+	addCard(card) {
+		super.addCard(card);
+		card.pos(0, (this.cards.length - 1) * 20);
 	}
 }
 
 class Foundation extends Pile {
-	constructor(suit) {
-		super();
-		this.suit = suit;
+	constructor(suit, parent, id) {
+		super(parent, id);
+		this.suit = Suit.fromString(suit);
+		this.element.innerHTML = this.suit.symbol;
+		this.element.classList.add('foundation');
+		this.element.classList.add(this.suit.name.toLowerCase());
 	}
 
 	addCard(card) {
-		if (this.isEmpty() && card.value === 'A') {
-			super.addCard(card);
-		} else if (this.topCard().value === 'K' || this.topCard().value === 'Q' || this.topCard().value === 'J' || this.topCard().value === '10' || this.topCard().value === '9' || this.topCard().value === '8' || this.topCard().value === '7' || this.topCard().value === '6' || this.topCard().value === '5' || this.topCard().value === '4' || this.topCard().value === '3' || this.topCard().value === '2') {
-			if (this.topCard().suit === card.suit && this.topCard().value === '2') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === '3' && card.value === '2') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === '4' && card.value === '3') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === '5' && card.value === '4') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === '6' && card.value === '5') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === '7' && card.value === '6') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === '8' && card.value === '7') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === '9' && card.value === '8') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === '10' && card.value === '9') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === 'J' && card.value === '10') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === 'Q' && card.value === 'J') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === 'K' && card.value === 'Q') {
-				super.addCard(card);
-			} else if (this.topCard().suit === card.suit && this.topCard().value === 'A' && card.value === 'K') {
-				super.addCard(card);
-			} else {
-				throw new Error('Invalid move');
-			}
+		if (card.suit == this.suit) {
+			throw new Error('The foundation accepts only ' + this.suit.name.toLowerCase());
 		}
+		if (this.isEmpty()) {
+			if (card.value.symbol != 'A') {
+				throw new Error('The foundation must start with an Ace');
+			}
+		} else if (this.topCard().value.rank != card.value.rank - 1) {
+			throw new Error('The foundation must be built in ascending order');
+		}
+		super.addCard(card);
+		card.pos(0, 0);
 	}
 }
 
 document.addEventListener('DOMContentLoaded', function() {
 	const table = document.getElementById('table');
-
+	// create a draw pile
+	const drawPile = new Pile(table, 'drawPile');
+	// create a discard pile
+	const discardPile = new Pile(table, 'discardPile');
 	// create a foundation for each suit
-	const heartsFoundation = new Foundation('Hearts');
-	const heartsFoundationElement = document.createElement('div');
-	heartsFoundationElement.classList.add('foundation');
-	heartsFoundationElement.innerHTML = heartsFoundation.toHtml();
-	table.appendChild(heartsFoundationElement);
-	const diamondsFoundation = new Foundation('Diamonds');
-	const diamondsFoundationElement = document.createElement('div');
-	diamondsFoundationElement.classList.add('foundation');
-	diamondsFoundationElement.innerHTML = diamondsFoundation.toHtml();
-	table.appendChild(diamondsFoundationElement);
-	const clubsFoundation = new Foundation('Clubs');
-	const clubsFoundationElement = document.createElement('div');
-	clubsFoundationElement.classList.add('foundation');
-	clubsFoundationElement.innerHTML = clubsFoundation.toHtml();
-	table.appendChild(clubsFoundationElement);
-	const spadesFoundation = new Foundation('Spades');
-	const spadesFoundationElement = document.createElement('div');
-	spadesFoundationElement.classList.add('foundation');
-	spadesFoundationElement.innerHTML = spadesFoundation.toHtml();
-	table.appendChild(spadesFoundationElement);
-
-
-	// create a 7 pile tableau
-	for (let i = 0; i < 7; i++) {
-		const pile = new Pile();
-		const pileElement = document.createElement('div');
-		pileElement.classList.add('pile');
-		pileElement.innerHTML = pile.toHtml();
-		table.appendChild(pileElement);
+	let foundation = {};
+	for (let suit of ['S', 'H', 'D', 'C']) {
+		const f = new Foundation(suit, table, `foundation${suit[0]}`);
+		foundation[suit] = f;
 	}
-
+	// create a 7 pile tableau
+	let pile = [];
+	for (let i = 1; i <= 7; i++) {
+		const t = new Tableau(table, `pile${i}`);
+		pile.push(t);
+	}
+	// create a deck
 	const deck = new Deck();
 	deck.shuffle();
-
-	for (let card of deck.cards) {
-		const cardElement = document.createElement('div');
-		cardElement.classList.add('card');
-		cardElement.innerHTML = card.toHtml();
-		cardElement.addEventListener('click', function() {
-			card.flip();
-			cardElement.innerHTML = card.toHtml();
-		});
-		const pileIndex = deck.cards.indexOf(card) % 7;
-		const pileElements = document.getElementsByClassName('pile');
-		pileElements[pileIndex].appendChild(cardElement);
-		cardElement.style.position = 'absolute';
-		cardElement.style.top = `${(pileElements[pileIndex].children.length - 1) * 30}px`;
-		// table.appendChild(cardElement);
+	// add cards to the tableau
+	for (let i = 1; i <= 7; i++) {
+		for (let j = i; j <= 7; j++) {
+			const card = deck.drawCard();
+			if (j === i) {
+				card.flip();
+			}
+			pile[j - 1].addCard(card);
+		}
 	}
+	// add remaining cards to the draw pile
+	while (deck.cards.length > 0) {
+		drawPile.addCard(deck.drawCard());
+	}
+
+	// add click event listener to draw pile
+	document.getElementById('drawPile').addEventListener('click', function() {
+		if (drawPile.isEmpty()) {
+			while (!discardPile.isEmpty()) {
+				let card = discardPile.removeCard();
+				card.flip();
+				drawPile.addCard(card);
+			}
+		}
+		let card = drawPile.removeCard();
+		card.flip();
+		discardPile.addCard(card);
+	});
+
+	// add a double click event listener to the discard pile
+	document.getElementById('discardPile').addEventListener('dblclick', function() {
+		let card = discardPile.topCard();
+		try {
+			foundation[card.suit.name[0]].addCard(card);
+			discardPile.removeCard();
+		} catch (e) {
+			console.log(e);
+		}
+	});
 });
